1e8fd3746fa941d34f7c25e2eefc61a4c0b04875
fcharte <francisco@fcharte.com>
2015-01-21 10:51:47 +0100

    Removed empty data module

--- Navigation/Navigation.dpr
+++ Navigation/Navigation.dpr
@@ -5,14 +5,12 @@ uses
   FMX.MobilePreview,
   FMX.Forms,
   Unit1 in 'Unit1.pas' {Form1},
-  Unit2 in 'Unit2.pas',
-  Unit3 in 'Unit3.pas' {DataModule3: TDataModule};
+  Unit2 in 'Unit2.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.CreateForm(TForm1, Form1);
-  Application.CreateForm(TDataModule3, DataModule3);
   Application.Run;
 end.
---
+++ Navigation/Unit3.pas
@@ -0,0 +1,25 @@
+unit Unit3;
+
+interface
+
+uses
+  System.SysUtils, System.Classes;
+
+type
+  TDataModule3 = class(TDataModule)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  DataModule3: TDataModule3;
+
+implementation
+
+{%CLASSGROUP 'FMX.Controls.TControl'}
+
+{$R *.dfm}
+
+end.
